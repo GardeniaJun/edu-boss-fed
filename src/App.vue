b3c56db7eb09c22c43f35aa3dfedf07db64fe89a
GardeniaJun <547630974@qq.com>
2022-02-07 20:58:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- 示例内容 -->
+    <h1>拉钩教育</h1>
+    <!-- 根路由出口，用于渲染路由组件 -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  // @import '~@/styles/variables.scss';
+
+  div {
+    background-color: $success-color;
+  }
+</style>
